3e6b50efbff71249c0fdfafbf56937a75529e1bc
asarichan <130378379+asarichan@users.noreply.github.com>
2023-10-25 10:42:33 +0000

    デザインの変更

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header bg-wed">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-wed-dark">
-          Welcome to <code>WED</code> Community.
+      <header className="App-header text-wed">
+        <img
+          src={
+            "https://4.bp.blogspot.com/-6nucQQvGF7M/WzC-BHQmAHI/AAAAAAABM90/mCILacrOAq8IqccOnH1U_umcAD56flJnQCLcBGAs/s800/yaruki_moeru_man.png"
+          }
+          className="App-logo"
+          alt="logo"
+        />
+        <p className="text-asari">
+          <code>WED</code>コミュニティにようこそ！
         </p>
         <a
-          className="text-wed-dark underline"
+          className="text-niku"
           href="https://wed.company/"
           target="_blank"
           rel="noopener noreferrer"
