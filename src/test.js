679e775640ec268bf280d92e953eb8cf9fbb3359
下山　碧 <135989997+aom1dor1@users.noreply.github.com>
2023-11-18 04:52:42 +0000

    test2

---
+++ src/test.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-title">
+        Artem
+        <header className="App-subtitle">
+          -教科書をリサイクルできるWebアプリ-
+        </header>
+      </header>
+
+      <header className="App-header">
+        <h1> Artem </h1>
+        <p>test</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
