6917f30955ebb133740eba475f16f9359850c7bc
Miginko <148962991+Miginko@users.noreply.github.com>
2023-11-18 16:30:59 +0900

    Create 出品yes or no.js

---
+++ src/出品yes or no.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div class="gorira">
+        <h2>本当に出品しますか？</h2>
+        <button class="example">はい</button>
+        <button class="example1">いいえ</button>
+      </div>
+    </div>
+  );
+}
