a000b1f9be6c65247700aa6225a7722050374104
Miginko <148962991+Miginko@users.noreply.github.com>
2023-11-18 16:33:43 +0900

    Create 商品成立.js

---
+++ src/商品成立.js
@@ -0,0 +1,12 @@
+mport "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div class="ua">
+        <h1>商品が成立しました</h1>
+        <a href="https://u-aizu.ac.jp">元のページに戻る</a>
+      </div>
+    </div>
+  );
+}
